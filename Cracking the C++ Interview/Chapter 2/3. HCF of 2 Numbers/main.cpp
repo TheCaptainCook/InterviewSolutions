5f82099ca3367c8e1000b7dc63da768fca00f57d
SHEIKH MASEM MANDAL <masem@duck.com>
2023-06-14 08:08:02 +0530

    Hcf Program commit

--- Cracking the C++ Interview/Chapter 2/3. HCF of 2 Numbers/main.cpp
+++ Cracking the C++ Interview/Chapter 2/3. HCF of 2 Numbers/main.cpp
@@ -1,3 +1,18 @@
+/*
+ * Write the C++ program to find out the HCF of 2 numbers.
+ *
+ * Concept - The full form of HCF is The Highest Common Factor.
+ *              HCF of two numbers is the highest factor that can divide the two numbers, evenly
+ *
+ * Ex - 16 -    2   2   2   2
+ * Ex - 18 -    2   3   3
+ * Selecting    -
+ * 
+ * HCF - 2  -   2
+ *
+ *  Written by TheCaptainCook
+ */
+
 #include <iostream>
 using namespace std;
 
