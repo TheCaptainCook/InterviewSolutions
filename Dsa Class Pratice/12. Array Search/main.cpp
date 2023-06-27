b56946d80c6206a781975932660c4a06b27a3421
SHEIKH MASEM MANDAL <masem@duck.com>
2023-06-27 08:48:45 +0530

    Initial Commit

--- Dsa Class Pratice/12. Array Search/main.cpp
+++ Dsa Class Pratice/12. Array Search/main.cpp
@@ -3,20 +3,33 @@ using namespace std;
 
 int main() {
 
-    char str[50], str1[50];
-    int i, count =0;
+    char str[50];
+    int i, count =0, j;
 
     cin>>str;
 
     for (i=0;str[i]!='\0';i++)
     {
-        count++;
+        count++;                // print the no of characters in name
     }
     cout<<count;
 
     for (i=count;i>=0;i--)
     {
-        cout<<str1[i];
+        cout<<str[i];           // Name in reverse order
+    }
+    cout<<endl;
+
+    for (i = 0, j='\0';i<count/2, j<=count/2;i++, j--)       //Palindrome checker
+    {
+        if (str[i]==str[j])
+        {
+            cout<<str[i]<<"-"<<str[j]<<"matched"<<endl;
+        }
+        else
+        {
+            cout<<str[i]<<"-"<<str[j]<<"Mot Matched"<<endl;
+        }
     }
 
 
---
+++ Dsa Class Pratice/13. Palindrome String/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   // convert the array into upper character and check for palindrome
+
+
+
+    return 0;
+}
